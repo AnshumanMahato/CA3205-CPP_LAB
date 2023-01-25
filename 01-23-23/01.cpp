a7e6f15fb8a29c922b97c08a11cd992653ff4b86
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2023-01-25 18:45:47 +0530

    ass1 q1 done

--- 01-23-23/01.cpp
+++ 01-23-23/01.cpp
@@ -1,66 +1,87 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-struct Date {
-	int day,month,year;
+struct Date
+{
+	int day, month, year;
 };
 
-struct KYC {
+struct KYC
+{
 	string name;
 	Date dob;
 	char pan[10];
 	string uid;
 	string add;
-	float income,saving;
-	
-	void input() {
-		cout<<"\nEnter name:"
-		gets(name);
-		cout<<"Enter Dob(dd mm yy):";
-		cin>>dob.day>>dob.month>>dob.year;
-		cout<<"Enter PAN:";
-		cin>>PAN;
-		cout<<"Enter Aadhar:";
-		cin>>uid;
-		cout<<"Enter address:";
-		gets(add);
-		cout<<"Enter Income:";
-		cin>>income;
-		do{
-			cout<<"Enter savings under tax scheme:";
-			cin>>saving;
-			
-			if(saving>150000)
-				cout<<"\nSavings cannot be above 1.5L\n";
-		}while(saving>150000)
+	float income, saving;
+
+	void input()
+	{
+		fflush(stdin);
+		cout << "\nEnter name:";
+		getline(cin, name);
+		cout << "Enter Dob(dd mm yy):";
+		cin >> dob.day >> dob.month >> dob.year;
+		cout << "Enter PAN:";
+		cin >> pan;
+		cout << "Enter Aadhar:";
+		cin >> uid;
+		fflush(stdin);
+		cout << "Enter address:";
+		getline(cin, add);
+		cout << "Enter Income:";
+		cin >> income;
+		do
+		{
+			cout << "Enter savings under tax scheme:";
+			cin >> saving;
+
+			if (saving > 150000)
+				cout << "\nSavings cannot be above 1.5L\n";
+		} while (saving > 150000);
 	}
-	
-	void ouput() {
-		cout<<"\nName:"<<name;
-		cout<<"\nDob(dd mm yy):"<<dob.day<<'-'<<dob.month<<'-'<<dob.year;
-		cout<<"\nPAN:"<<pan;
-		cout<<"\nAadhar:"<<uid;
-		cout<<"\nAddress:"<<add
-		cout<<"\nAnnual Income:"<<income;
-		cout<<"\nSavings under tax scheme:"<<saving;
+
+	void ouput()
+	{
+		cout << "\nName:" << name;
+		cout << "\nDob(dd mm yy):" << dob.day << '-' << dob.month << '-' << dob.year;
+		cout << "\nPAN:" << pan;
+		cout << "\nAadhar:" << uid;
+		cout << "\nAddress:" << add;
+		cout << "\nAnnual Income:" << income;
+		cout << "\nSavings under tax scheme:" << saving;
 	}
-	
-	float computeTax(){
-		if(income <= 500000)
-			retrun 0;
-		else if(income <=1000000)
-			return 0.1*(income-saving);
+
+	float computeTax()
+	{
+		if (income <= 500000)
+			return 0;
+		else if (income <= 1000000)
+			return 0.1 * (income - saving);
 		else
-			return 0.1*(income-saving);
+			return 0.1 * (income - saving);
 	}
 };
 
-int main(){
-	const int size=2;
+int main()
+{
+	const int size = 7;
 	KYC list[size];
-	cout<<"Enter Detais:\n";
-	for(int i=0;i<size;i++){
-		cout<<"\n"
+	cout << "Enter " << size << " Detais:\n";
+	for (int i = 0; i < size; i++)
+	{
+		cout << "\nKYC " << i + 1;
+		list[i].input();
 	}
+
+	cout << "\nKYC Detais:\n";
+	for (int i = 0; i < size; i++)
+	{
+		cout << "\n\nKYC " << i + 1;
+		list[i].ouput();
+		cout << "\nIncome Tax:" << fixed << setprecision(2) << list[i].computeTax();
+	}
+
+	return 0;
 }
