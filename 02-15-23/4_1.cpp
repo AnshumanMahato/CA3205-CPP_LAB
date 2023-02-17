cd8a136ff102779e2bec06d23fda09d51c21c632
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2023-02-17 11:04:05 +0530

    stack half

--- 02-15-23/4_1.cpp
+++ 02-15-23/4_1.cpp
@@ -4,23 +4,29 @@ using namespace std;
 
 class IntStack
 {
-    int top;
+    int top, size;
     int *stk;
 
 public:
     IntStack()
     {
         top = -1;
+        size = 10;
         stk = new int[10];
     }
     IntStack(int size)
     {
         top = -1;
+        this->size = size;
         stk = new int[size];
     }
 
     IntStack(const IntStack &stack)
     {
         top = stack.top;
+        size = stack.size;
+        stk = new int[size];
+        for (int i = 0; i < top; i++)
+            stk[i] = stack.stk[i];
     }
 };
